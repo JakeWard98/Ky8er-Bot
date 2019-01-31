29f243e7ca59965c0c2a361ddd1e3f8b956ef735
Jake Ward <jakeward9815@gmail.com>
2019-01-31 22:14:38 +0000

    Console Message text change

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ const token = 'NTQwMTAwMjg5NTIxMjU0NDEy.DzM60w.r6U6RCNxW2nNccWGNY7lrJCEqag'
 bot.on('ready', function(){
     //Shows that bot is ready for use and what has actually started
     console.log("\n> Started");
-    console.log("> Erebus Bot is Ready too Use\n")
+    console.log("> Ky8er Bot is Ready too Use\n")
 })
 
 bot.on('message', function(message){
