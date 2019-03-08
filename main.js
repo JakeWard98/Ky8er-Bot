1bfc7b45c4b4c63e9b179bfdf2038f9a40fc33ca
Jake Ward <jakeward9815@gmail.com>
2019-03-08 13:19:12 +0000

    Bot now has "Now PLaying" when on and fixed lines

--- main.js
+++ main.js
@@ -15,8 +15,10 @@ client.auth = auth;
 client.on('ready', function(){
     //Shows that bot is ready for use and what has actually started
     console.log("\n> Started");
-    console.log(`> ${client.user.username} is Online & Ready too Use\n`)
-})
+    console.log(`> ${client.user.username} is Online & Ready too Use`);
+    client.user.setActivity('^help');
+    console.log("> Activity Has Been Set\n");
+});
 
 client.on('message', function(message){
     //test message
@@ -27,14 +29,14 @@ client.on('message', function(message){
     //test message
     if(message.content == "Ping")
     {
-        message.channel.send("Pong")
+        message.channel.send("Pong");
     }
     //test message
     if(message.content == "General Kenobi")
     {
-        message.channel.send("YOU ARE A BOLD ONE")
+        message.channel.send("YOU ARE A BOLD ONE");
     }
-})
+});
 
 //logging the bot in
 client.login(auth.token);
