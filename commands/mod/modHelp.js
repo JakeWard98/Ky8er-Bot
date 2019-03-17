88c86854e8df69aa19059b3b3e5bc08dafec76e4
Jake Ward <jakeward9815@gmail.com>
2019-03-17 23:29:51 +0000

    Embeded the Messages

---
+++ commands/mod/modHelp.js
@@ -0,0 +1,32 @@
+const Discord = require('discord.js-commando');
+const embed = require('discord.js');
+
+class Help extends Discord.Command{
+	constructor(client){
+		super(client,{
+			name: 'modhelp',
+			group: 'mod',
+			memberName: 'help',
+			description: 'Help command to show commands'
+		});
+	}
+
+	async run (message, args){
+		// message.channel.send('For help use ^help\nThese are the current commands:```\n^help\n^ping\n^hello\n^server\n^numbers```');
+		var helpEmbed = new embed.RichEmbed()
+			.setColor('#9013FE')
+            .setTitle('Mod Help')
+            .addField('Prefix', 'The Prefix is ^')
+            .addField('help', 'Calls the user Help Dialogue box')
+            .addField('modHelp', 'Calls this Help Dialogue Box')
+			.addField('hello', 'Greets the User')
+            .addField('server', 'Shows the Server Name')
+            .addField('numbers', 'Shows Total Members')
+            .addField('ping', 'Pings the bot and show the average "Heartbeat" (Can be very temperamental)')
+            .setTimestamp()
+            .setFooter('Bot Still in Development')
+		message.channel.send(helpEmbed);
+	}
+}
+
+module.exports = Help;
--- commands/user/help.js
+++ commands/user/help.js
@@ -1,7 +1,29 @@
-module.exports = {
-	name: 'help',
-	description: 'HelpS',
-	execute(message, args) {
-		message.channel.send('For help use ^help\nThese are the current commands:```\n^help\n^ping\n^hello\n^server\n^numbers```');
-	},
-};
+const Discord = require('discord.js-commando');
+const embed = require('discord.js');
+
+class Help extends Discord.Command{
+	constructor(client){
+		super(client,{
+			name: 'help',
+			group: 'user',
+			memberName: 'help',
+			description: 'Help command to show commands'
+		});
+	}
+
+	async run (message, args){
+		// message.channel.send('For help use ^help\nThese are the current commands:```\n^help\n^ping\n^hello\n^server\n^numbers```');
+		var helpEmbed = new embed.RichEmbed()
+			.setColor('#F810EB')
+			.setTitle('Help')
+			.addField('Prefix', 'The Prefix is ^')
+			.addField('help', 'Calls this Dialogue box')
+			.addField('hello', 'Greets the User')
+			.addField('server', 'Shows the Server Name')
+			.setTimestamp()
+			.setFooter('Bot Still in Development')	
+		message.channel.send(helpEmbed);
+	}
+}
+
+module.exports = Help;
