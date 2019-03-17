1f7e51090fb8f2df03024ddae491fc3b7535d455
Jake Ward <jakeward9815@gmail.com>
2019-03-17 23:29:08 +0000

    Fixed

--- commands/mod/ping.js
+++ commands/mod/ping.js
@@ -1,12 +1,23 @@
-//CANT FIND CLIENT SO PING IS CALLED FROM INSIDE THE MAIN FILE FOR NOW
-//If anyone wants to fix please do
-//don't change the name, i will do that
+const Discord = require('discord.js-commando');
+const client = new Discord.Client()
 
+const { token } = require('../../config.json');
 
-/*module.exports = {
-	name: 'broke',
-	description: 'Test for user to see if the bot is Online',
-	execute(message, args) {
-		message.channel.send(`${client.user.username} is Online & Ready too Use`);
-	},
-};*/
+class ServerName extends Discord.Command{
+	constructor(client){
+		super(client,{
+			name: 'ping',
+			group: 'mod',
+			memberName: 'ping',
+			description: 'Shows Server ping'
+		});
+	}
+
+	async run (message, args){
+		message.channel.send(`${client.user.username} is Online & Ready To Use @ ` + `${client.ping} ms`);
+	}
+}
+
+module.exports = ServerName;
+
+client.login(token);
