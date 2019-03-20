ac31123910bb907f49c0565c18f98e7aa5d0c208
Jake Ward <jakeward9815@gmail.com>
2019-03-20 19:42:36 +0000

    Small Fix

--- commands/voice/play.js
+++ commands/voice/play.js
@@ -28,10 +28,10 @@ class Play extends Discord.Command{
 		if(message.member.voiceChannel){
 			if(!message.guild.voiceConnection){
 				if(!servers[message.guild.id]){
-					servers[message.message.guild.id] = { queue: []}
+					servers[message.message.guild.id] = { queue: [] }
 				}
 				message.member.voiceChannel.join()
-				.then(connection =>{
+				.then(connection => {
 					var server = servers[message.guild.id];
 					// message.reply('Playing: ' + ytdl.getInfo(URL));
 					server.queue.push(args);
